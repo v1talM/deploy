a78beb1b27a3345641b7722216111c5e9f5a298c
牟秋宇 <QiuyuMou@sf-express.com>
2019-04-02 19:33:25 +0800

    引入jquery、node-saas

---
+++ src/components/index/Navigator.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper blue">
+                <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#"><i class="material-icons">search</i></a></li>
+                    <li><a href="#"><i class="material-icons">view_module</i></a></li>
+                    <li><a href="#"><i class="material-icons">refresh</i></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Navigator"
+    }
+</script>
+
+<style scoped>
+
+</style>
