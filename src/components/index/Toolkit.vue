a81f65acda2fce2daae04b0f26c54cd6cb785a71
牟秋宇 <QiuyuMou@sf-express.com>
2019-04-02 20:02:07 +0800

    调整主题颜色

--- src/components/index/Toolkit.vue
+++ src/components/index/Toolkit.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="fixed-action-btn">
-        <a class="btn-floating btn-large red">
-            <i class="large material-icons">mode_edit</i>
+        <a class="btn-floating btn-large red z-depth-3">
+            <i class="large material-icons">dashboard</i>
         </a>
         <ul>
-            <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-            <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-            <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-            <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+            <li><a class="btn-floating grey z-depth-3" title="设置项目模块"><i class="material-icons">toc</i></a></li>
+            <li><a class="btn-floating yellow darken-1 z-depth-3" title="一键设置模块分支"><i class="material-icons">mode_edit</i></a></li>
+
         </ul>
     </div>
 </template>
