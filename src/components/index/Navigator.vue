1b89a9508827bee83e7399c6e7562211fced6b5c
牟秋宇 <QiuyuMou@sf-express.com>
2019-05-16 16:58:11 +0800

    调整图标

--- src/components/index/Navigator.vue
+++ src/components/index/Navigator.vue
@@ -2,12 +2,13 @@
     <div class="navbar-fixed">
         <nav>
             <div class="nav-wrapper blue">
-                <router-link :to="{name: 'index'}" class="brand-logo">{{ emoji[Math.floor(Math.random() * emoji.length)] }}</router-link>
+                <router-link :to="{name: 'index'}" class="brand-logo tooltipped" data-position="bottom" data-delay="50"
+                             data-tooltip="点击返回首页" title="点击返回首页">{{ emoji[Math.floor(Math.random() * emoji.length)] }}</router-link>
                 <ul class="right hide-on-med-and-down">
                     <!--<li><a href="#"><i class="material-icons">search</i></a></li>-->
                     <li><router-link :to="{name: 'mini-app'}" class="tooltipped"
                                      data-position="bottom" data-delay="50"
-                                     data-tooltip="小程序列表" title="小程序列表"><i class="material-icons">view_module</i> </router-link></li>
+                                     data-tooltip="小程序列表" title="小程序列表">提审小程序</router-link></li>
                     <!--<li><a href="#"><i class="material-icons">refresh</i></a></li>-->
                 </ul>
             </div>
