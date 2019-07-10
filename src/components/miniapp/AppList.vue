8fe00c7ace41ba0f1d48114fce0b2f5338cd5025
牟秋宇 <QiuyuMou@sf-express.com>
2019-07-10 16:16:47 +0800

    增加店铺二维码

--- src/components/miniapp/AppList.vue
+++ src/components/miniapp/AppList.vue
@@ -1,32 +1,43 @@
 <template>
-    <table class="striped centered">
-        <thead>
-        <tr>
-            <th data-field=""></th>
-            <th data-field="id">小程序名称</th>
-            <th data-field="template_id">当前模板ID</th>
-            <th data-field="app_id">APP-ID</th>
-            <th data-field="shop_id">SHOP-ID</th>
-            <th data-field="shop_type">店铺类型</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="app in appList">
-            <td><p>
-                <input name="shop_id" type="radio"
-                       v-model="selectApp"
-                       :value="app.wx_name + ' - ' + app.shop_id + ' - ' + app.shop_type + ' - ' + app.app_id"
-                       :id="app.shop_id" />
-                <label :for="app.shop_id"></label>
-            </p></td>
-            <td>{{ app.wx_name }}</td>
-            <td>{{ app.template_id }}</td>
-            <td>{{ app.app_id }}</td>
-            <td>{{ app.shop_id }}</td>
-            <td>{{ shopTypeMap[app.shop_type] }}</td>
-        </tr>
-        </tbody>
-    </table>
+    <div>
+        <table class="striped centered">
+            <thead>
+            <tr>
+                <th data-field=""></th>
+                <th data-field="id">小程序名称</th>
+                <th data-field="template_id">当前模板ID</th>
+                <th data-field="app_id">APP-ID</th>
+                <th data-field="shop_id">SHOP-ID</th>
+                <th data-field="shop_type">店铺类型</th>
+                <th data-field="shop_qrcode">二维码</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="app in appList">
+                <td><p>
+                    <input name="shop_id" type="radio"
+                           v-model="selectApp"
+                           :value="app.wx_name + ' - ' + app.shop_id + ' - ' + app.shop_type + ' - ' + app.app_id"
+                           :id="app.shop_id" />
+                    <label :for="app.shop_id"></label>
+                </p></td>
+                <td>{{ app.wx_name }}</td>
+                <td>{{ app.template_id }}</td>
+                <td>{{ app.app_id }}</td>
+                <td>{{ app.shop_id }}</td>
+                <td>{{ shopTypeMap[app.shop_type] }}</td>
+                <td>
+                    <a class="btn-floating blue lighten-4" data-target="qrcode_modal" @click="qrcode_modal(app.qr_code)"><i class="material-icons">zoom_in</i></a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div id="qrcode_modal" class="qrcode_modal modal">
+            <div class="modal-content">
+                <img class="materialboxed" width="650" :src="active_qrcode">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -42,7 +53,8 @@
                     0: '通用版',
                     1: '餐饮版',
                     2: '聚合版'
-                }
+                },
+                active_qrcode: '',
             }
         },
         mounted () {
@@ -56,10 +68,23 @@
             selectApp () {
                 this.$store.dispatch('setSelectApp', this.selectApp)
             }
+        },
+        methods: {
+            qrcode_modal(url) {
+                let $ = window.$
+                $(".modal").modal()
+                if (url === undefined) {
+                    this.active_qrcode = ''
+                } else {
+                    this.active_qrcode = url
+                }
+            }
         }
     }
 </script>
 
-<style scoped>
-
+<style lang="sass">
+    .card .card-image img
+        width: 320px
+        display: inline-block
 </style>
---
+++ src/pages/SetModuleBranch.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container moduleconf-container">
+        <div class="row">
+            <div class="col s12 m12">
+                <module-conf />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ModuleConf from '@/components/setmodulebranch/ModuleConf'
+    export default {
+        name: "SetModuleBranch",
+        components: {
+            ModuleConf
+        }
+    }
+</script>
+
+<style lang="sass">
+
+</style>
